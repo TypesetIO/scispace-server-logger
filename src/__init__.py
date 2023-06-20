ff56724d745b4211b84e5e7d4232e85f7ca3e084
srinathk <srinath@typeset.io>
2023-06-20 16:59:08 +0530

    Setup the package proper config

--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-from .scispace_logger import ScispaceLogger
---
+++ src/scispace_logger/logger/__init__.py
@@ -0,0 +1,10 @@
+"""
+    Logger Service related class for scispace app
+"""
+from .base import BaseServerLogger
+
+
+class ScispaceLogger(BaseServerLogger):
+    """
+        Logger Service to be used while logging for all scispace related apps
+    """
