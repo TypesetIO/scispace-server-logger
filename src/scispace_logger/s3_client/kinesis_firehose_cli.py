d53bb2e722f006ba047b87d2924a1bcc31359055
srinathk <srinath@typeset.io>
2023-06-21 15:09:54 +0530

    handle boto3 error

--- src/scispace_logger/s3_client/kinesis_firehose_cli.py
+++ src/scispace_logger/s3_client/kinesis_firehose_cli.py
@@ -14,8 +14,12 @@ class KinesisFirehoseClient(object):
     def __init__(self) -> None:
         aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
         aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-        self._cli = boto3.client(
-            'firehose', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        try:
+            self._cli = boto3.client(
+                'firehose', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        except Exception as e:
+            print('Exception while establishing boto3 connection: ', str(e))
+            self._cli = None
 
     def push_record(self, **kwargs):
         """
