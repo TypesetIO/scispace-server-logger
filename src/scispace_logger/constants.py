0012f30146bc41afdf4e57157940dc617882323d
srinathk <srinath@typeset.io>
2023-12-18 12:39:16 +0530

    Support for multiple clients from same logger

--- src/scispace_logger/constants.py
+++ src/scispace_logger/constants.py
@@ -4,6 +4,16 @@
 
 import os
 
+TRUE_VALUES = ['1', 'true']
+
+def return_bool(string):
+    """
+        Util Function to return env variable bool
+    """
+    if string.lower() in TRUE_VALUES:
+        return True
+    return False
+
 # LOG LEVEL RELATED CONSTANTS
 ERROR = 'ERROR'
 INFO = 'INFO'
@@ -25,10 +35,11 @@ AWS_REGION_NAME = os.getenv('AWS_REGION_NAME', 'us-west-2')
 # Client Related CONSTANTS
 ENV = os.getenv('SCISPACE_LOGGER_ENV')
 APP_NAME = os.getenv('SCISPACE_LOGGER_APP_NAME')
-ENABLE_SCISPACE_LOGGER = os.getenv('ENABLE_SCISPACE_LOGGER')
-ENABLE_SCISPACE_EVENT_HANDLERS = os.getenv('ENABLE_SCISPACE_EVENT_HANDLERS')
+ENABLE_SCISPACE_LOGGER = return_bool(os.getenv('ENABLE_SCISPACE_LOGGER'))
+ENABLE_SCISPACE_EVENT_HANDLERS = return_bool(os.getenv('ENABLE_SCISPACE_EVENT_HANDLERS'))
 
 # EVENT ANALYTICS CONSTANTS
+DEFAULT_LOGGER = os.getenv('DEFAULT_LOGGER', 'apps.log')
 VENV_LOCATION = os.getenv('VENV_LOCATION', 'env3')
 DEFAULT_IGNORED_FILES = [VENV_LOCATION] if VENV_LOCATION else []
 DEFAULT_IGNORED_CLASSES = ['AxiomHandler', 'HostnameFilter']
@@ -40,5 +51,4 @@ IGNORED_CLASSES = os.getenv('IGNORED_CLASSES')
 IGNORED_CLASSES = IGNORED_CLASSES.split(',') if IGNORED_CLASSES else []
 IGNORED_CLASSES += DEFAULT_IGNORED_CLASSES
 IGNORED_CLASSES = [cls.strip() for cls in IGNORED_CLASSES]
-IGNORED_FUNCTIONS = ['__str__']
-DEFAULT_LOGGER = os.getenv('DEFAULT_LOGGER', 'apps.log')
+IGNORED_FUNCTIONS = ['__str__', '__repr__']
